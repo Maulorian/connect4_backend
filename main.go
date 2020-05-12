00469f77b350371a028342b74190ce521e1032db
blabla <alexandredp@apptweak.com>
2020-05-12 18:28:32 +0200

    Added middleware

--- main.go
+++ main.go
@@ -42,10 +42,10 @@ func getMove(c *gin.Context) {
 }
 func CORSMiddleware(c *gin.Context) {
 	fmt.Println("Sending cors headers")
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(200)
